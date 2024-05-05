8981d8ac077ae3ec41a05867768f55ee6c683a07
Julio Vidaurre <julio.vidaurre@gmail.com>
2024-05-05 19:00:28 -0400

    Clear screen

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,11 +1,38 @@
+#define VIDEO_ADDRESS 0xb8000
+#define MAX_ROWS 25
+#define MAX_COLS 80
+#define SCREEN_SIZE (MAX_ROWS * MAX_COLS)
+#define WHITE_ON_BLACK 0x0f
+
+typedef struct {
+    int x;
+    int y;
+} Cursor;
+
+Cursor cursor;
+
+void print_char_at(char c, int row, int col)
+{
+    short *video_memory = (short *)VIDEO_ADDRESS;
+    int offset = ((row * MAX_COLS) + col);
+    video_memory[offset] = (WHITE_ON_BLACK << 8 | c);
+}
+
+void clear_screen()
+{
+    short *video_memory = (short *)VIDEO_ADDRESS;
+    for (int i = 0; i < SCREEN_SIZE; i++) {
+        video_memory[i] = 0;
+    }
+}
 
 void bootmain()
 {
-    // char *video_memory = (char *)0xb8000;
+    clear_screen();
+    
+    cursor.x = 0;
+    cursor.y = 10;
+    print_char_at('X', 0, 10);
 
-    // *video_memory = 'X';
-    short *video_memory = (short *)0xb8000;
-    for (int i = 0; i < 20; i++) {
-        *(video_memory + i) = (0x0f << 8 | 'X');
-    }
+    // print_char_at('A', cursor.y, cursor.x);
 }
