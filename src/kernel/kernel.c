1190e6d48912a7faf6df11e255f133e9d10ce163
Julio Vidaurre <julio.vidaurre@gmail.com>
2024-05-06 18:15:10 -0400

    Print strings, print to the next line when write pass the rightmost character and scroll the screen

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -9,13 +9,72 @@ typedef struct {
     int y;
 } Cursor;
 
-Cursor cursor;
+static Cursor cursor;
 
-void print_char_at(char c, int row, int col)
+void scroll_screen()
+{
+    // Copy memory to the previous line
+    short *video_memory = (short *)VIDEO_ADDRESS;
+    int i;
+    for (i = 0; i < (SCREEN_SIZE - MAX_COLS); i++) {
+        video_memory[i] = video_memory[i + MAX_COLS];
+    }
+
+    // Clear any leftover char in the last line
+    for (; i < SCREEN_SIZE; i++) {
+        video_memory[i] = 0;
+    }
+}
+
+void set_cursor_next_line(Cursor *cursor)
+{
+    cursor->x = 0;
+    cursor->y++;
+    if (cursor->y >= MAX_ROWS) {
+        cursor->y = MAX_ROWS - 1; // Set cursor at last line;
+        scroll_screen();
+    }
+}
+
+void advance_cursor(Cursor *cursor)
+{
+    cursor->x++;
+    if (cursor->x >= MAX_COLS) {
+        set_cursor_next_line(cursor);
+    }
+}
+
+
+void print_char_at(char c, int attribute, int row, int col)
 {
     short *video_memory = (short *)VIDEO_ADDRESS;
+    if (attribute == 0) {
+        attribute = WHITE_ON_BLACK;
+    }
+
     int offset = ((row * MAX_COLS) + col);
-    video_memory[offset] = (WHITE_ON_BLACK << 8 | c);
+    video_memory[offset] = (attribute << 8 | c);
+}
+
+void print_char(char c)
+{
+    switch (c)
+    {
+        case '\n': {
+            set_cursor_next_line(&cursor);
+        } break;
+        default: {
+            print_char_at(c, WHITE_ON_BLACK, cursor.y, cursor.x);
+            advance_cursor(&cursor);
+        } break;
+    }
+}
+
+void print_string(char *string)
+{
+    while (*string) {
+        print_char(*string++);
+    }
 }
 
 void clear_screen()
@@ -31,8 +90,7 @@ void bootmain()
     clear_screen();
     
     cursor.x = 0;
-    cursor.y = 10;
-    print_char_at('X', 0, 10);
+    cursor.y = 20;
 
-    // print_char_at('A', cursor.y, cursor.x);
+    print_string("1 Hello World!\n");
 }
